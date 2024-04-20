684fb67d72defd67cf0524112f2b8d0deaf2288e
anthonycs4 <165516654+anthonycs4@users.noreply.github.com>
2024-04-19 20:14:57 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import subprocess
+
+def ejecutar_comando():
+    comando = "shiny run --reload --launch-browser app_dir/app.py"
+    proceso = subprocess.run(comando, shell=True, capture_output=True, text=True)
+
+    # Verifica si hubo errores
+    if proceso.returncode != 0:
+        print("Ocurrió un error al ejecutar el comando:")
+        print(proceso.stderr)
+    else:
+        print("El comando se ejecutó correctamente.")
+        print("Salida:")
+        print(proceso.stdout)
+
+# Llamada a la función para ejecutar el comando
+ejecutar_comando()
